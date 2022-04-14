2aea3fed939aabbaec7d6279e7462439e3e99d1c
chowhp <chowhp71@gmail.com>
2022-04-14 08:31:51 +0000

    add in model

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,1 @@
-from .program import main, print_page, select_traintest_ratio, create_traintest_sample, sample_cleanup, gaussian_nb, random_forest_classifier, decision_tree_classifier, logistic_regression
+from .program import main, print_page, select_traintest_ratio, create_traintest_sample, sample_cleanup, model
